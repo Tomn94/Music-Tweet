8311647111a7945cd75becfe98bfe30de680fe10
tomn94 <tmnstudios@hotmail.fr>
2017-05-24 15:23:00 +0200

    + Session Connectivity on Apple Watch

--- Watch-Tweet Extension/InterfaceController.swift
+++ Watch-Tweet Extension/InterfaceController.swift
@@ -7,28 +7,86 @@
 //
 
 import WatchKit
-import Foundation
-
+import WatchConnectivity
 
 class InterfaceController: WKInterfaceController {
     
     @IBOutlet var nowPlayingLabel: WKInterfaceLabel!
     @IBOutlet var tweetBtn: WKInterfaceButton!
     @IBOutlet var artwork: WKInterfaceImage!
+    
+    var session: WCSession? {
+        didSet {
+            if let session = session {
+                session.delegate = self
+                session.activate()
+            }
+        }
+    }
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
+        session = WCSession.default()
         reset()
     }
-
+    
     @IBAction func tweet() {
+        
+        session?.sendMessage(["action" : "tweet"],
+                             replyHandler: nil) { error in
+                                
+                                self.presentAlert(withTitle: "Unable to tweet current track",
+                                                  message: error.localizedDescription,
+                                                  preferredStyle: .alert,
+                                                  actions: [WKAlertAction(title: "Cancel", style: .cancel) {}])
+        }
     }
     
     @IBAction func reset() {
+        
+        session?.sendMessage(["get" : "info"],
+                             replyHandler: { info in
+                                
+                                let text = info["text"] as? String
+                                self.nowPlayingLabel.setText(text)
+                                self.artwork.setImage(info["artwork"] as? UIImage)
+                                self.tweetBtn.setEnabled(!(text?.isEmpty ?? true))
+                                
+        }) { error in
+            self.presentAlert(withTitle: "Unable to get current track from iPhone",
+                              message: error.localizedDescription,
+                              preferredStyle: .alert,
+                              actions: [WKAlertAction(title: "Cancel", style: .cancel) {}])
+        }
     }
     
     @IBAction func artworkActivationChanged(_ value: Bool) {
+        
+        session?.sendMessage(["setArworkOn" : value],
+                             replyHandler: nil) { error in
+                                
+                                self.presentAlert(withTitle: "Unable to update artwork setting",
+                                                  message: error.localizedDescription,
+                                                  preferredStyle: .alert,
+                                                  actions: [WKAlertAction(title: "Cancel", style: .cancel) {}])
+        }
+    }
+    
+}
+
+
+extension InterfaceController: WCSessionDelegate {
+    
+    func session(_ session: WCSession,
+                 activationDidCompleteWith activationState: WCSessionActivationState,
+                 error: Error?) {
+        if let error = error {
+            presentAlert(withTitle: "Unable to connect to iPhone",
+                         message: error.localizedDescription,
+                         preferredStyle: .alert,
+                         actions: [WKAlertAction(title: "Cancel", style: .cancel) {}])
+        }
     }
     
 }
