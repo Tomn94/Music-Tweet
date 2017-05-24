4b6d2aef462dbff7615562f8c1056e83ad95b6e1
tomn94 <tmnstudios@hotmail.fr>
2017-05-24 15:38:23 +0200

    + Base Watch Connectivity Handler on iOS

---
+++ Music Tweet/ConnectivityHandler.h
@@ -0,0 +1,14 @@
+//
+//  ConnectivityHandler.h
+//  Music Tweet
+//
+//  Created by Tomn on 24/05/2017.
+//  Copyright © 2017 U969H3GXLU. All rights reserved.
+//
+
+@import UIKit;
+@import WatchConnectivity;
+
+@interface ConnectivityHandler : NSObject <WCSessionDelegate>
+
+@end
---
+++ Music Tweet/ConnectivityHandler.m
@@ -0,0 +1,30 @@
+//
+//  ConnectivityHandler.m
+//  Music Tweet
+//
+//  Created by Tomn on 24/05/2017.
+//  Copyright © 2017 U969H3GXLU. All rights reserved.
+//
+
+#import "ConnectivityHandler.h"
+
+@implementation ConnectivityHandler
+
+- (void)               session:(WCSession *)session
+activationDidCompleteWithState:(WCSessionActivationState)activationState
+                         error:(NSError *)error
+{
+    
+}
+
+- (void) sessionDidBecomeInactive:(WCSession *)session
+{
+    
+}
+
+- (void) sessionDidDeactivate:(WCSession *)session
+{
+    
+}
+
+@end
